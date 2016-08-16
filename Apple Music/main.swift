7fb23d72fb3595203b3cb7d9f28c4253ca8abb5f
Ricardo Jenez <rrj@altiscale.com>
2016-08-16 14:54:03 -0700

    Initial Commit

---
+++ Apple Music/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Apple Music
+//
+//  Created by Ricardo Jenez on 8/16/16.
+//  Copyright © 2016 Ricardo Jenez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
